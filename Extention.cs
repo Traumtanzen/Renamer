2dfe242e6cc6da90f0fc9348a8a75d1d0029727b
sgvardeenko <s.gvardeenko@gmail.com>
2020-04-30 15:53:54 +0300

    Add project files.

---
+++ Extention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+namespace Renamer
+{
+    public static class ListExt
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        static extern int StrCmpLogicalW(string lhs, string rhs);
+
+        public static void SortNatural<T>(this List<T> self, Func<T, string> stringSelector)
+        {
+            self.Sort((lhs, rhs) => StrCmpLogicalW(stringSelector(lhs), stringSelector(rhs)));
+        }
+
+        public static void SortNatural(this List<string> self)
+        {
+            self.Sort(StrCmpLogicalW);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfNames = File.ReadAllLines(args[0]).ToList();
+
+            DirectoryInfo d = new DirectoryInfo(args[1]);
+            List<FileInfo> folder = d.GetFiles().ToList();
+
+            folder = folder.Where(x => x.Name != "desktop.ini").ToList();
+
+            folder.SortNatural(item => item.Name);
+
+            if (folder.Count != listOfNames.Count)
+            {
+                Console.WriteLine("Different count of files and names. Exiting.");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                for (int i = 0; i < listOfNames.Count(); i++)
+                {
+                    File.Move(folder[i].FullName, folder[i].FullName.Replace(folder[i].Name, listOfNames[i] + ".wav"));
+                }
+                Console.WriteLine("Success! Exiting.");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
